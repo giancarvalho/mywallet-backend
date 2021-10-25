2405d32a6816fa191ba0b70e726db7f2c272e8d5
giancarvalho <giandcarvalho@gmail.com>
2021-10-25 13:20:17 -0300

    edited grammar mistake in description of the third test

--- tests/signIn.test.js
+++ tests/signIn.test.js
@@ -4,7 +4,6 @@ import app from "../src/app";
 describe("GET /sign-in", () => {
     it("should return 400 if email or password is missing", async () => {
         const body = { email: "testonaldo@example.com" };
-
         const result = await supertest(app).post("/sign-in").send(body);
 
         expect(result.status).toEqual(400);
@@ -15,20 +14,17 @@ describe("GET /sign-in", () => {
             email: "ThisUserDoesntExist@fail.com",
             password: 123456,
         };
-
         const result = await supertest(app).post("/sign-in").send(body);
 
         expect(result.status).toEqual(404);
     });
 
-    it("should an object with user name and token if credentials are valid", async () => {
+    it("should get an object with user name and token if credentials are valid", async () => {
         const body = {
             email: "testonaldo@example.com",
             password: "asdfgh",
         };
-
         const result = await supertest(app).post("/sign-in").send(body);
-
         const testUser = {
             name: "Testonaldo",
             token: "1765c602-20c3-4081-8ca4-d5a19ba94f93",
