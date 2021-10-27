3899e04a3b349697181f4081570e095bb3ca07d8
giancarvalho <giandcarvalho@gmail.com>
2021-10-27 12:51:48 -0300

    added beforeAll and afterAll statements in order for tests not rely on pre existing data

--- tests/signIn.test.js
+++ tests/signIn.test.js
@@ -1,7 +1,34 @@
 import supertest from "supertest";
+import { pool } from "../db/pool";
 import app from "../src/app";
+import bcrypt from "bcrypt";
 
 describe("GET /sign-in", () => {
+    let id;
+    beforeAll(async () => {
+        try {
+            id = (
+                await pool.query(
+                    "INSERT INTO users (name, email) VALUES ('teste', 'test@test.com') RETURNING id;"
+                )
+            ).rows[0].id;
+
+            await pool.query(
+                `INSERT INTO passwords ("userId", password) VALUES ($1, $2)`,
+                [id, bcrypt.hashSync("123456", 10)]
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    afterAll(async () => {
+        await pool.query(`DELETE FROM users WHERE id = $1;`, [id]);
+
+        await pool.query(`DELETE FROM passwords WHERE "userId" = $1;`, [id]);
+        pool.end();
+    });
+
     it("should return 400 if email or password is missing", async () => {
         const body = { email: "testonaldo@example.com" };
         const result = await supertest(app).post("/sign-in").send(body);
@@ -19,17 +46,14 @@ describe("GET /sign-in", () => {
         expect(result.status).toEqual(404);
     });
 
-    it("should get an object with user name and token if credentials are valid", async () => {
+    it("should get an object properties token and name if credentials are valid", async () => {
         const body = {
-            email: "testonaldo@example.com",
-            password: "asdfgh",
+            email: "test@test.com",
+            password: "123456",
         };
         const result = await supertest(app).post("/sign-in").send(body);
-        const testUser = {
-            name: "Testonaldo",
-            token: "1765c602-20c3-4081-8ca4-d5a19ba94f93",
-        };
 
-        expect(result.body).toMatchObject(testUser);
+        expect(result.body).toHaveProperty("name");
+        expect(result.body).toHaveProperty("token");
     });
 });
