34beb98670e9a01b9e57777acbb84394eca3a13a
Sidney Outeiro <sid@dcc.ufrj.br>
2021-10-27 11:39:57 -0300

    preparando para o review

--- db/pool.js
+++
@@ -1,9 +0,0 @@
-import pg from "pg";
-//delete line below and import your db configs
-import settings from "./settings.js";
-
-const { Pool } = pg;
-
-const pool = new Pool(settings);
-
-export { pool };
--- src/server.js
+++
@@ -1,4 +0,0 @@
-import app from "./app.js";
-const PORT = 4000;
-
-app.listen(PORT);
